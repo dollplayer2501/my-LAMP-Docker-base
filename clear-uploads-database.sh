f447ea84e6206fd0a88c51ec4450bed889ebffbf
Morry Masahiko <morry@duelist.org>
2021-12-20 10:14:37 +0900

    add image test PHP 3

--- clear-uploads-database.sh
+++ clear-uploads-database.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+\find ./html/uploads -type f | \grep -v -E './html/uploads/.gitkeep' | \xargs \rm -rf
+
 \find ./mysql/database -type d | \grep -v -E './mysql/database$' | \xargs \rm -rf
 \find ./mysql/database -type f | \grep -v -E './mysql/database/.gitkeep' | \xargs \rm -rf
